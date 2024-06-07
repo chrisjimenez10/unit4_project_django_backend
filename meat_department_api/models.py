aefa255fd02ca2198f935c3ca52c83cb20585a97
chris <jimenezchristopher5@gmail.com>
2024-06-07 16:23:16 -0500

    fixed serializers.py file by removing packaged field from Meta class

--- meat_department_api/models.py
+++ meat_department_api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Meat(models.Model):
@@ -7,7 +8,7 @@ class Meat(models.Model):
     type = models.CharField(max_length=32)
     description = models.CharField(max_length=50)
     origin = models.CharField(max_length=50)
-    packaged = models.DateTimeField()
+    packaged = models.DateTimeField(default=timezone.now)
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
