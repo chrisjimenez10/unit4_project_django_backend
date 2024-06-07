0c7363a1142f05e10776227679304af6a58cdda1
chris <jimenezchristopher5@gmail.com>
2024-06-07 12:03:07 -0500

    changed url naming convention for snacks_api/urls.py from api/contacts to api/snacks

--- snacks_api/urls.py
+++ snacks_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/contacts', views.SnacksList.as_view(), name='snacks_list'),
-    path('api/contacts/<int:pk>', views.SnacksDetail.as_view(), name='snacks_detail'),
+    path('api/snacks', views.SnacksList.as_view(), name='snacks_list'),
+    path('api/snacks/<int:pk>', views.SnacksDetail.as_view(), name='snacks_detail'),
 ]
