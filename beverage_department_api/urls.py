fce001861a712897680cc2bd84336384da247f22
chris <jimenezchristopher5@gmail.com>
2024-06-07 11:59:41 -0500

    fixed bugs on beverage_department_api/urls.py with spelling of urlpatterns and changed url to api/beverages. fixed bugs on produce_api/serializers.py with plural spelling of fields in Meta class because it was singular

--- beverage_department_api/urls.py
+++ beverage_department_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlPatterns = [
-    path('beverages/', views.BeveragesList.as_view(), name='beverages_list'),
-    path('beverages/<int:pk>/', views.BeverageDetail.as_view(), name='beverage_detail'),
+urlpatterns = [
+    path('api/beverages', views.BeveragesList.as_view(), name='beverages_list'),
+    path('api/beverages/<int:pk>', views.BeverageDetail.as_view(), name='beverage_detail'),
 ]
--- unit4_project_django_backend/settings.py
+++ unit4_project_django_backend/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'grocery_store_api',
-        'HOST': 'localhost',
+        # 'HOST': 'localhost',
     }
 }
 
